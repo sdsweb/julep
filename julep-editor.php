2e64f87636183f61a1c82ec0e84b6c10da44d983
Matt <matt@slocumstudio.com>
2015-10-28 12:47:03 -0400

    Fixing enqueue issues.

--- julep-editor.php
+++ julep-editor.php
@@ -18,7 +18,9 @@ if(!class_exists('wp_julep_editor'))
         */
         public function __construct()
         {
-			$this->julep_editor_files_enqueue();
+			//$this->julep_editor_files_enqueue();
+			add_action( 'wp_enqueue_styles','julep_editor_enqueue_scripts' );
+			add_action( 'wp_enqueue_scripts','julep_editor_enqueue_scripts' );
         }
 
         /**
@@ -38,18 +40,25 @@ if(!class_exists('wp_julep_editor'))
         } 
 		
         /**
-        * Include Default Scripts and styles
+        * Include Default styles
         */  
-        public function julep_editor_files_enqueue()
+        public function julep_editor_enqueue_style()
         {
 			wp_enqueue_style( 'julep_css', plugins_url( 'css/julep.css' , __FILE__ ));
+			wp_enqueue_style( 'wp-color-picker' );
+        }
+		
+		/**
+        * Include Default Scripts
+        */  
+        public function julep_editor_enqueue_scripts()
+        {
 			wp_enqueue_script('jquery-ui-core');  
 			wp_enqueue_script('jquery');
 			wp_enqueue_script('jquery-ui-resizable');
 			wp_enqueue_script('jquery-ui-draggable');
 			wp_enqueue_script('jquery-ui-selectable');
 			wp_enqueue_script( 'wp-color-picker' ); 
-			wp_enqueue_style( 'wp-color-picker' );
 			
 			wp_enqueue_script('jquery.ui.rotatable', plugins_url( 'js/jquery.ui.rotatable.js' , __FILE__ ), '', '', true );
 			wp_enqueue_script('qunit', 'http://code.jquery.com/qunit/qunit-1.12.0.js', '', '', true );
